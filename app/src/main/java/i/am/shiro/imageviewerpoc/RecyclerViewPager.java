64e12ad4b7292d0c707d11f13c8c6a92a41d09a5
Subaru "Shiro" Tashiro <wightwulf1944@gmail.com>
2019-03-12 21:10:04 +0800

    clarified RecyclerViewPager access modifiers

--- app/src/main/java/i/am/shiro/imageviewerpoc/RecyclerViewPager.java
+++ app/src/main/java/i/am/shiro/imageviewerpoc/RecyclerViewPager.java
@@ -3,32 +3,29 @@ package i.am.shiro.imageviewerpoc;
 import android.support.v7.widget.PagerSnapHelper;
 import android.support.v7.widget.RecyclerView;
 
+@SuppressWarnings("WeakerAccess")
 public final class RecyclerViewPager {
 
-    final PagerSnapHelper pagerSnapHelper = new PagerSnapHelper();
+    private final PagerSnapHelper pagerSnapHelper = new PagerSnapHelper();
 
-    final RecyclerView recyclerView;
+    private final RecyclerView recyclerView;
 
-    boolean pagingEnabled;
+    private boolean pagingEnabled;
 
-    @SuppressWarnings("WeakerAccess")
     public RecyclerViewPager(RecyclerView recyclerView) {
         this.recyclerView = recyclerView;
     }
 
-    @SuppressWarnings("WeakerAccess")
     public void enablePaging() {
         pagerSnapHelper.attachToRecyclerView(recyclerView);
         pagingEnabled = true;
     }
 
-    @SuppressWarnings("WeakerAccess")
     public void disablePaging() {
         pagerSnapHelper.attachToRecyclerView(null);
         pagingEnabled = false;
     }
 
-    @SuppressWarnings("WeakerAccess")
     public void togglePaging() {
         if (pagingEnabled) {
             disablePaging();
