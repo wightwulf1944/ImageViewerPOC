83cd429a04312d43112db94238e94993fe4bafa9
Subaru "Shiro" Tashiro <subaru.tashiro@novare.com.hk>
2019-04-08 18:59:38 +0800

    merged dummy MainActivity and MainFragment together

--- app/src/main/java/i/am/shiro/imageviewerpoc/activities/MainActivity.java
+++ app/src/main/java/i/am/shiro/imageviewerpoc/activities/MainActivity.java
@@ -1,21 +1,36 @@
 package i.am.shiro.imageviewerpoc.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
-import i.am.shiro.imageviewerpoc.fragments.MainFragment;
+import java.util.Arrays;
+import java.util.List;
+
+import i.am.shiro.imageviewerpoc.R;
+import i.am.shiro.imageviewerpoc.util.BundleManager;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final List<String> images = Arrays.asList("a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .add(android.R.id.content, new MainFragment())
-                    .commit();
-        }
+        View fab = findViewById(R.id.fab_launch);
+        fab.setOnClickListener(v -> launchViewer());
+    }
+
+    private void launchViewer() {
+        BundleManager manager = new BundleManager();
+        manager.setUrisStr(images); // TODO - consider using java.net.Uri instead
+
+        Intent viewer = new Intent(this, ImageViewerActivity.class);
+        viewer.putExtras(manager.getBundle());
+
+        startActivity(viewer);
     }
 }
