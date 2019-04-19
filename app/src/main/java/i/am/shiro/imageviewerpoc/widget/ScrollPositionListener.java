95f9d26321e80486296b62d06c59669ca57e8b93
Subaru "Shiro" Tashiro <subaru.tashiro@novare.com.hk>
2019-04-19 19:57:10 +0800

    support vertical scroll page detection

--- app/src/main/java/i/am/shiro/imageviewerpoc/widget/ScrollPositionListener.java
+++ app/src/main/java/i/am/shiro/imageviewerpoc/widget/ScrollPositionListener.java
@@ -16,6 +16,14 @@ public final class ScrollPositionListener extends RecyclerView.OnScrollListener 
     public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
         float extent = recyclerView.computeHorizontalScrollExtent();
         float offset = recyclerView.computeHorizontalScrollOffset();
+        if (extent == 0 && offset == 0) {
+            extent = recyclerView.computeVerticalScrollExtent();
+            offset = recyclerView.computeVerticalScrollOffset();
+        }
+        if (extent == 0 && offset == 0) {
+            return;
+        }
+        
         int currentPosition = Math.round(offset / extent);
         onPositionChangeListener.accept(currentPosition);
     }
