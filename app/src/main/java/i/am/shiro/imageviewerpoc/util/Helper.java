8f9dd1037fda4adfed03aeaff14425f015c5fd37
Subaru "Shiro" Tashiro <subaru.tashiro@novare.com.hk>
2019-04-23 01:00:05 +0800

    implement hiding statusbar on older api levels

--- app/src/main/java/i/am/shiro/imageviewerpoc/util/Helper.java
+++ app/src/main/java/i/am/shiro/imageviewerpoc/util/Helper.java
@@ -3,14 +3,21 @@ package i.am.shiro.imageviewerpoc.util;
 import android.app.Activity;
 import android.os.Build;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class Helper {
 
     public static void setStatusBarButtonsVisibility(Activity activity, boolean visible) {
-        // SYSTEM_UI_FLAG_IMMERSIVE is only available since Kitkat. Hiding the system UI
-        // without this flag is pointless, as any screentap would show the system UI again...
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            View decorView = activity.getWindow().getDecorView();
+        Window window = activity.getWindow();
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
+            if (!visible) {
+                window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+            } else {
+                window.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+            }
+        } else {
+            View decorView = window.getDecorView();
             int uiOptions;
             if (visible) {
                 uiOptions = View.SYSTEM_UI_FLAG_VISIBLE
@@ -24,5 +31,4 @@ public class Helper {
             decorView.setSystemUiVisibility(uiOptions);
         }
     }
-
 }
