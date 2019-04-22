eac2c80f87ace104d8d3369b63d7489abc0ed8b9
Robb <zeugmaonline@hotmail.com>
2019-04-22 21:01:46 +0200

    Fix system UI not limiting image area when zooming

--- app/src/main/java/i/am/shiro/imageviewerpoc/fragments/ImagePagerFragment.java
+++ app/src/main/java/i/am/shiro/imageviewerpoc/fragments/ImagePagerFragment.java
@@ -252,13 +252,19 @@ public class ImagePagerFragment extends Fragment implements GoToPageDialogFragme
         } else {
             int uiOptions;
             if (visible) {
-                uiOptions = View.SYSTEM_UI_FLAG_VISIBLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+                uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
             } else {
                 uiOptions = View.SYSTEM_UI_FLAG_IMMERSIVE
+                        // Set the content to appear under the system bars so that the
+                        // content doesn't resize when the system bars hide and show.
                         | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        // Hide the nav bar and status bar
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN;
             }
             Objects.requireNonNull(getView()).setSystemUiVisibility(uiOptions);
         }
